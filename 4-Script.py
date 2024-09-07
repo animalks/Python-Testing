cb8c5ae50ded3f07256f1126075ae6e2615311d0
animalks <ksleeanimal@gmail.com>
2024-09-07 09:22:20 -0400

    Delete files

--- 4-Script.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-WORD = {}
-while True:
-    n = str(input())
-    if n == "enddd" :
-        break
-    WORD[n] = WORD.get(n, 0) + 1
-print (WORD)
-
--- 5-Script.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-WORD = ()
-while True:
-    n = str(input())
-    if n == "enddd" :
-        break
-    WORD = WORD + (n, )
-print (WORD)
-print (WORD[1])
